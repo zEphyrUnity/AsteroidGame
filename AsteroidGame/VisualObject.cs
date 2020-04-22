eed10807ddb0b12331494057e0f8fc4f8a5a1ca2
Igor <61108251+zEphyrUnity@users.noreply.github.com>
2020-04-22 23:35:44 +0300

    Коммит 3. Добавлен класс визуального объекта.

---
+++ AsteroidGame/VisualObject.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Drawing;
+
+namespace AsteroidGame
+{
+    class VisualObject
+    {
+        protected Point _Position;
+        protected Point _Direction;
+        protected Size _Size;
+
+        public VisualObject(Point Position, Point Direction, Size Size)
+        {
+            _Position = Position;
+            _Direction = Direction;
+            _Size = Size;
+        }
+
+        public void Draw(Graphics g)
+        {
+            g.DrawEllipse(Pens.RoyalBlue, _Position.X, _Position.Y, _Size.Width, _Size.Height);
+        }
+
+        public void Update()
+        {
+            _Position.X += _Direction.X;
+            _Position.Y += _Direction.Y;
+
+            if (_Position.X < 0)
+                _Direction.X *= -1;
+
+            if (_Position.Y < 0)
+                _Direction.Y *= -1;
+
+            if (_Position.X > Game.Width || _Position.Y > Game.Height)
+                _Direction.X *= -1;
+        }
+    }
+}
