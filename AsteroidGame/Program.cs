a839a989006be602ff21433de52824395e493ab8
Igor <61108251+zEphyrUnity@users.noreply.github.com>
2020-05-01 14:57:30 +0300

    Рефакторинг кода(добавление констант)

--- AsteroidGame/Program.cs
+++ AsteroidGame/Program.cs
@@ -16,9 +16,12 @@ namespace AsteroidGame
             Application.SetCompatibleTextRenderingDefault(false);
 
             Form game_form = new Form();
+            const ushort game_form_width = 800;
+            const ushort game_form_height = 600;
 
-            game_form.Width = 800;
-            game_form.Height = 600;
+
+            game_form.Width = game_form_width;
+            game_form.Height = game_form_height;
          
             game_form.Show();
 
